d3b1837d3b456663768efb27d4efb0a24580b793
Swopnilraj <114748377+Swopnilraj@users.noreply.github.com>
2026-06-22 09:03:10 +0545

    OS project

---
+++ gdt.h
@@ -0,0 +1,36 @@
+#ifndef __GDT_H
+#define __GDT_H
+#include "types.h"
+	class GlobalDescriptorTable
+	{
+	    public:
+		    class SegmentDescriptor
+		{
+		private:
+			uint16_t limit_lo;
+			uint16_t base_lo;
+			uint16_t base_hi;
+			uint16_t type;
+			uint16_t flags_limit_hi;
+			uint16_t base_vhi;
+		public:
+			SegmentDescriptor(uint32_t base, uint32_t limit, uint16_t type);
+			uint32_t Base();
+			uint32_t Limit();
+
+		} __attribute__((packed));
+
+
+		SegmentDescriptor nullSegmentSelector;
+		SegmentDescriptor unusedSegmentSelector;
+		SegmentDescriptor codeSegmentSelector;
+		SegmentDescriptor dataSegmentSelector;
+
+	public:
+		GlobalDescriptorTable();
+		~GlobalDescriptorTable();
+
+		uint16_t CodeSegmentSelector();
+		uint16_t DataSegmentSelector();
+    };    
+#endif
